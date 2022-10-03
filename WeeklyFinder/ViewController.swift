6b40e0a4e1de0c1a6fe612aa07efc8d9f8336066
Alex Kudanov <sanyasanya312@gmail.com>
2022-10-03 17:55:52 +0300

    Add code

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultButton.layer.cornerRadius = 12
     }
 
-
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var montxTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLable: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dateTF.text, let month = montxTF.text, let year = yearTF.text else {
+            return
+        }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else {
+            return
+        }
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        resultLable.text = weekday
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
